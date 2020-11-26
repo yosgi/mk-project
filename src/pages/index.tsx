3b7e20543821327dbfa56ba09026eb57e2cea1a2
yosgi <18667911647yosgi@gmail.com>
2020-11-26 22:24:02 +0800

    更新首页

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button, Carousel, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default () => {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Carousel>
         <Carousel.Item>
           <img
@@ -44,17 +44,56 @@ export default () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <section>
-        <Row className="justify-content-md-center">
-          <Col md="auto">
+      <section style={{ margin: '40px 0' }} className="container">
+        <Row>
+          <Col md={{ span: 4, offset: 4 }}>
             <h1>Our Works</h1>
           </Col>
         </Row>
         <Row className="justify-content-md-center">
-          <Col xs={6}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-            purus nisl. Morbi vitae vulputate enim, in sagittis nisi. Etiam
-            luctus odio odio nulla sed.
+          <Col md={{ span: 4, offset: 4 }}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
+              purus nisl. Morbi vitae vulputate enim, in sagittis nisi. Etiam
+              luctus odio odio nulla sed. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Donec non purus nisl. Morbi vitae
+              vulputate enim, in sagittis nisi. Etiam luctus odio odio nulla
+              sed.
+            </p>
+          </Col>
+        </Row>
+      </section>
+      <section className={styles.imgContainer}>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <div className={styles.shadowContainer}>
+              <a></a>
+              <img src={require('../assets/WechatIMG236.jpeg')}></img>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.shadowContainer}>
+              <a></a>
+              <img src={require('../assets/WechatIMG237.jpeg')}></img>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.shadowContainer}>
+              <a></a>
+              <img src={require('../assets/WechatIMG237.jpeg')}></img>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            {/* <a>cover image</a> */}
+            <img src={require('../assets/WechatIMG236.jpeg')}></img>
+          </Col>
+          <Col xs={4}>
+            <img src={require('../assets/WechatIMG237.jpeg')}></img>
+          </Col>
+          <Col xs={4}>
+            <img src={require('../assets/WechatIMG238.jpeg')}></img>
           </Col>
         </Row>
       </section>
