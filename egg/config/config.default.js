6760a33d870cfeee6a1d6b9ddedf9e3038a3da51
yosgi <18667911647yosgi@gmail.com>
2020-11-28 12:00:51 +0800

    内网穿透演示需要

---
+++ egg/config/config.default.js
@@ -0,0 +1,50 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+const path = require('path');
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1606534224156_3488';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  config.view = {
+    mapping: {
+      '.html': 'nunjucks',
+    },
+  };
+
+  config.assets = {
+    publicPath: '/public/',
+    devServer: {
+      debug: true,
+      command: 'umi dev',
+      port: 8000,
+      env: {
+        APP_ROOT: process.cwd() + '/app/web',
+        BROWSER: 'none',
+        ESLINT: 'none',
+        SOCKET_SERVER: 'http://127.0.0.1:8000',
+        PUBLIC_PATH: 'http://127.0.0.1:8000',
+      },
+    },
+  };
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
---
+++ egg/config/plugin.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+exports.assets = {
+  enable: true,
+  package: 'egg-view-assets',
+};
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+};
