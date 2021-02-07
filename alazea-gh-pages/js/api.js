ce74784ba2837d6a5d57e0d797fa70cc9457a5ba
Fu Chenxu <fuchenxu2008@163.com>
2021-02-07 16:53:31 +0800

    i18n in progress

--- alazea-gh-pages/js/api.js
+++ alazea-gh-pages/js/api.js
@@ -1,8 +1,8 @@
 var token = sessionStorage.getItem('token');
 (function ($) {
   'use strict';
-  //var baseUrl = 'http://127.0.0.1:3001'
-  var baseUrl = 'http://minmakeitnow.com/'
+  var baseUrl = 'http://127.0.0.1:3001'
+  // var baseUrl = 'http://minmakeitnow.com/'
   function api(args) {
     if (args.data && typeof args.data === 'object' && !!token) {
       args.data.token = token
@@ -13,9 +13,9 @@ var token = sessionStorage.getItem('token');
     var data = args.data;
     var success = args.success
     var error = args.error
-  
+
     $.ajax({
-      url: baseUrl + '/api' + url, 
+      url: baseUrl + '/api' + url,
       type: method,
       success: function (json) {
         if (json.code === 500) {
@@ -23,19 +23,19 @@ var token = sessionStorage.getItem('token');
             sessionStorage.removeItem('token')
             showLogin()
             $.sendWarning('超时，请重新登录', 3000,function () {
-              
+
             });
           }
         }  else {
           success(json)
-        } 
-       
+        }
+
       },
       data:data,
       error: error,
       contentType,
       dataType: 'json',
-      
+
     })
   }
   window.api = api
---
+++ alazea-gh-pages/js/locale.js
@@ -0,0 +1,30 @@
+const DEFAULT_LANG = 'en'
+const LOCALE_KEY = 'localeLanguage'
+
+const en = $.getJSON({ url:'../locales/en.json', async: false }).responseJSON
+const zh = $.getJSON({ url:'../locales/zh.json', async: false }).responseJSON
+
+const locales = {
+  en,
+  zh
+}
+
+const i18n = new VueI18n({
+  locale: DEFAULT_LANG,
+  messages: locales
+})
+
+const switchLocale = lang => {
+  if (lang === undefined) {
+    lang = window.localStorage.getItem(LOCALE_KEY)
+    if (locales[lang] === undefined) {
+      lang = DEFAULT_LANG
+    }
+  }
+  window.localStorage.setItem(LOCALE_KEY, lang)
+
+  Vue.config.lang = lang
+  i18n.locale = lang
+}
+
+switchLocale()
